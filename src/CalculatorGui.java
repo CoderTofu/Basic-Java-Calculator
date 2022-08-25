c9156b58b250670ae6311cf28e28ff66026d2c00
Paolo <vioneverforget@gmail.com>
2022-08-25 08:46:31 +0800

    Added buttons
    However, the commands for this button are yet to be added.

--- src/CalculatorGui.java
+++ src/CalculatorGui.java
@@ -4,19 +4,21 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class CalculatorGui {
-    private JFrame mainFrame; // Our main window
+    private static JFrame mainFrame = new JFrame("Calculator");
     private JPanel display_panel, keys_panel;
 
-    public static void main(String[] args) {
+    public static void main() {
         CalculatorGui calculatorGui = new CalculatorGui();
-        calculatorGui.showGridBagLayoutDemo();
+        calculatorGui.KeyLayout();
+
+        mainFrame.setVisible(true);
     }
 
     public CalculatorGui() {
-        prepareGUI();
+        PrepareGUI();
     }
 
-    private void prepareGUI() {
+    private void PrepareGUI() {
         mainFrame = new JFrame("Calculator");
         display_panel = new JPanel();
         keys_panel = new JPanel();
@@ -30,12 +32,75 @@ public class CalculatorGui {
 
         mainFrame.setSize(400, 600);
         mainFrame.add(display_panel, BorderLayout.NORTH);
-        mainFrame.add(keys_panel, BorderLayout.SOUTH);
-        mainFrame.setVisible(true);
     }
 
-    private void showGridBagLayoutDemo() {
-        keys_panel.setLayout(new GridLayout(4, 4));
+    private void KeyLayout() {
+        GridBagLayout layout = new GridBagLayout();
+        keys_panel.setSize(400, 500);
+        keys_panel.setLayout(layout);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        keys_panel.add(new JButton("÷"), gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        keys_panel.add(new JButton("x"), gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        keys_panel.add(new JButton("-"), gbc);
+
+        // Here we add our number keys
+        KeyNums(gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 1;
+        gbc.gridheight = 2;
+        keys_panel.add(new JButton("+"), gbc);
+
+        gbc.gridx = 3;
+        gbc.gridy = 3;
+        gbc.gridheight = 2;
+        keys_panel.add(new JButton("="), gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 2;
+        gbc.gridheight = 1;
+        keys_panel.add(new JButton("0"), gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        gbc.gridwidth = 1;
+        keys_panel.add(new JButton("."), gbc);
+
+        mainFrame.add(keys_panel, BorderLayout.SOUTH);
+    }
+    
+    private void KeyNums(GridBagConstraints gbc) {
+        // our x and y values for "1" key
+        int x = 0;
+        int y = 3;
+        for (int i = 1; i < 10; i++) {
+            gbc.gridx = x;
+            gbc.gridy = y;
+            gbc.gridwidth = 1;
+            keys_panel.add(new JButton(Integer.toString(i)), gbc);
+
+            // Adjustments for next number
+            if (i % 3 != 0) {
+                x = x + 1;
+            } else {
+                x = 0;
+                y = y - 1;
+            }
+        }
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        CalculatorGui calculator = new CalculatorGui();
+        CalculatorGui.main();
     }
 }
